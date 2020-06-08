c9acf0269666889f1cda7e4a58d2981fe4107a65
kao37 <4107937@gmail.com>
2020-06-08 23:34:00 +0900

    add-ユーザー単体テスト途中

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
-describe User do
+describe 'user登録テスト', model: User do 
   describe '#create' do
+
+    it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
     it "nicknameがない場合は登録できないこと" do
       user = build(:user, nickname: nil)
@@ -15,5 +20,31 @@ describe User do
       expect(user.errors[:email]).to include("can't be blank")
     end
 
+    it "passwordがない場合は登録出来ないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+      it " 重複したemailが存在する場合は登録できないこと " do
+        user = create(:user)
+        another_user = build(:user, email: user.email)
+        another_user.valid?
+        expect(another_user.errors[:email]).to include("has already been taken")
+      end
+
+      it " passwordが7文字以上であれば登録できること " do
+        user = build(:user, password: "0000000", password_confirmation: "0000000")
+        expect(user).to be_valid
+      end
+  
+ 
+      end
+    end
   end
-end
