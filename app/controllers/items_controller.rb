18cbad1f241f6a7b132ff3209da99ded8e2f0b3f
kimkim0814 <tnryj505@gmail.com>
2020-06-08 19:36:17 +0900

    create items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  private
+
+  def set_categories
+    @categories      = CategoryIndex.all
+    @bigcategories   = Bigcategory.all
+    @smallcategories = Smallcategory.all
   end
 end
