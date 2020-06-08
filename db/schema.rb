eced896e38f0a7c6ebb8055677698e2a38f626b5
s-ss-sss <64889991+s-ss-sss@users.noreply.github.com>
2020-06-08 18:12:25 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -28,15 +28,18 @@ ActiveRecord::Schema.define(version: 2020_06_08_082910) do
     t.string "name", null: false
     t.integer "price", null: false
     t.string "description", null: false
-    t.string "user_id", null: false
-    t.string "category_id", null: false
-    t.string "brand_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "brand_id", null: false
     t.string "size", null: false
     t.integer "status", null: false
     t.string "cost", null: false
     t.string "days", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_items_on_brand_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,4 +60,7 @@ ActiveRecord::Schema.define(version: 2020_06_08_082910) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "brands"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end
