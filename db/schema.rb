04e623882d69199080631ffb980382c60e358870
kimkim0814 <tnryj505@gmail.com>
2020-06-08 17:30:56 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_063757) do
+ActiveRecord::Schema.define(version: 2020_06_08_082910) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2020_06_08_063757) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "ancestry"
-    t.string "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
