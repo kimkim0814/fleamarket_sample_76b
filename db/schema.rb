7788eddd8d557837ebd073870564947a46319634
kimkim0814 <tnryj505@gmail.com>
2020-06-09 19:36:19 +0900

    fix_migration

--- db/schema.rb
+++ db/schema.rb
@@ -57,25 +57,6 @@ ActiveRecord::Schema.define(version: 2020_06_09_040312) do
     t.index ["item_id"], name: "index_images_on_item_id"
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.string "description", null: false
-    t.bigint "user_id", null: false
-    t.bigint "category_id", null: false
-    t.bigint "brand_id", null: false
-    t.string "size", null: false
-    t.integer "status", null: false
-    t.string "cost", null: false
-    t.string "days", null: false
-    t.date "sold_day"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["brand_id"], name: "index_items_on_brand_id"
-    t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
     t.string "password", null: false
@@ -94,7 +75,4 @@ ActiveRecord::Schema.define(version: 2020_06_09_040312) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "items", "brands"
-  add_foreign_key "items", "categories"
-  add_foreign_key "items", "users"
 end
