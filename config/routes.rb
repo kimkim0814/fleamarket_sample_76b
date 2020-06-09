6d90822b82d025908c93f5da64eb0b52d2f61e4e
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-09 19:57:52 +0900

    header途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions',
   }
   devise_scope :user do
     get 'identifications', to: 'users/registrations#new_identification'
     post 'identifications', to: 'users/registrations#create_identification'
-  end
-  devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
