60bd0f87847d16ab7b2797138acfa611bdd08b91
kao37 <4107937@gmail.com>
2020-06-09 18:37:44 +0900

    add-マイページ見た目完結

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,4 +2,11 @@ class MypagesController < ApplicationController
   def index
   end
 
+  def card
+  end
+
+  def logout
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
   get 'mypages/index'
+  get 'mypages/logout'
+  get 'mypages/card'
 end
