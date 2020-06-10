c0b38914d91f331b4facd8edae8504124d1d0698
kimkim0814 <tnryj505@gmail.com>
2020-06-10 20:30:32 +0900

    items_show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,20 @@ class ItemsController < ApplicationController
     @items = Item.includes(:images).order(updated_at: "DESC")
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @comment = Comment.new
+    @comments = @item.comments
+  end
   private
 
   def set_categories
     @categories      = CategoryIndex.all
     @bigcategories   = Bigcategory.all
     @smallcategories = Smallcategory.all
+  end
+  
+  def item_params
+    params.require(:item).permit(:name, :category_id,:days,:price, :brand_id, :explanation,images: [:image, :_destroy, :id])
   end
 end
