963e4fd558f3b6a2f805f2da1b7517ca9ea68dd4
kimkim0814 <tnryj505@gmail.com>
2020-06-10 21:45:35 +0900

    com

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
 private
 
 def comment_params
-  params.require(:comment).permit(:comment).merge(user_id: current_user.id, item_id: params[:item_id])
+  params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
 end
 
 end
