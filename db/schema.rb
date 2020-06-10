11a012811ba6541696e88f5a7cbc8394d9b239f6
kimkim0814 <tnryj505@gmail.com>
2020-06-10 11:34:23 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_040312) do
+ActiveRecord::Schema.define(version: 2020_06_09_112404) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postcode", null: false
@@ -77,21 +77,16 @@ ActiveRecord::Schema.define(version: 2020_06_09_040312) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password", null: false
     t.string "nickname", null: false
-    t.string "familyname", null: false
-    t.string "firstname", null: false
-    t.string "familyname_kana", null: false
-    t.date "birth_date", null: false
-    t.integer "postcode", null: false
-    t.integer "prefecture_code", null: false
-    t.string "address_city", null: false
-    t.string "address_street", null: false
-    t.string "address_building"
-    t.integer "phone_number"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "items", "brands"
