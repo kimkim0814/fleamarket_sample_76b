b9982f9330a80c8be558bb01c6af970a0d9d4d27
kao37 <4107937@gmail.com>
2020-06-11 20:12:44 +0900

    add-パンくずマイページ完

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,3 +6,18 @@ crumb :mypages do
   link "マイページ", mypages_index_path
   parent :root
 end
+
+crumb :logout do 
+  link "ログアウト", mypages_logout_path
+  parent :mypages
+end
+
+crumb :card do 
+  link "支払方法",  mypages_card_path
+  parent :mypages
+end
+
+# crumb :card do 
+#   link "クレジットカード情報入力", mypage_credit_register_path 
+#   parent :credit_enter
+# end
