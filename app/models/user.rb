8019bc4c37ae2af24e40d755378d392fbe0a69a1
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-11 16:39:32 +0900

    omniauth_comtroller編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,20 @@ class User < ApplicationRecord
   validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
   has_many :items
   has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
 end
