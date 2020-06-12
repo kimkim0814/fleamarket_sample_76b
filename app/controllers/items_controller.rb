860df3e1921f1e4b6d371a6dcdbd9b6f6afdfe9c
kimkim0814 <tnryj505@gmail.com>
2020-06-12 16:01:22 +0900

    条件分け

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :set_item,only:[:show,:destroy,:edit]
   def index
     @items = Item.includes(:images).order(updated_at: "DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
+
     @comment = Comment.new
 
   end
@@ -16,7 +17,7 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.destroy
-      redirect_to user_path
+      redirect_to root_path
     else
       redirect_to item_path
     end
@@ -31,5 +32,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :category_id,:days,:price, :brand_id, :explanation,images: [:image, :_destroy, :id])
+  end
+
+  def set_item
+      @item = Item.find(params[:id])
   end
 end
