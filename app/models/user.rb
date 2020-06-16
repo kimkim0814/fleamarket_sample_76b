ad80e02b797bb7e3dc476c1be1a9d03542e83015
kao37 <4107937@gmail.com>
2020-06-16 14:32:56 +0900

    add-登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one :identification       
   has_one :address
-  has_one :cards
+  has_one :card
   accepts_nested_attributes_for :identification
   accepts_nested_attributes_for :address
   validates :nickname,presence: true
