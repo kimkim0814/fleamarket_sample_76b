08bb272647ba4a494381808239c87fee0e9b2134
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-17 00:35:22 +0900

    js修正2

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -14,22 +14,10 @@ $(function(){
     }
   );
 
-  // $(".category__parent").hover(
-  //   function () {
-  //     $(".category__child").show(); 
-  //     // $(this).append($(".category__child"));
-  //   },
-  //   function () {
-  //     $(".category__child").hide();
-  //   }
-  // );
-
   $(".category__parent").hover(
     function () {
-      var idname = $(this).attr('id');
-      // console.log(idname);
-      
-      var child = $("#"+ idname).find('.category__child');
+      var id = $(this).attr('id');
+      var child = $("#"+ id).find('.category__child');
      
       $(child).show(); 
     },
@@ -40,15 +28,9 @@ $(function(){
 
   $(".category__child").hover(
     function () {
-      var parent_id = $(this).parents('.category__parent').attr('id');
-      var child_id = $(this).attr('id');
-      // console.log(parent_id);
-      // console.log(child_id);
-      // var child = $("#"+ parent_id).find('.category__child')
-      var grandchild = $("#"+ child_id).find(".category__grandchild");
+      var id = $(this).attr('id');
+      var grandchild = $("#"+ id).find(".category__grandchild");
       console.log(grandchild.length)
-      // console.log('.grandchild--'+parent_id+'/'+child_id)
-      // $('.grandchild--'+parent_id+'/'+child_id).show(); 
       $(grandchild).show(); 
     },
     function () {
