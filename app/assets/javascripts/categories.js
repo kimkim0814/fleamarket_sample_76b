65b85acedd8f4b459a82d9053ac391026c0f4593
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-16 19:35:11 +0900

    css追加

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,25 +1,23 @@
 
-
 $(function(){
 
-  // $(".category__parent").hide(); 
+  $(".category__parent").hide(); 
   $(".category__child").hide(); 
   $(".category__grandchild").hide(); 
 
+  $(".left-box--first").hover(
+    function () {
+      $(".category__parent").show(); 
+    },
+    function () {
+      $(".category__parent").hide();
+    }
+  );
 
-  // $(".category--header").hover(
-  //   function () {
-  //     $(this).append($(".category__parent"));
-  //     $(".category__parent").show(); 
-  //   },
-  //   function () {
-  //     $(".category__parent").hide();
-  //   }
-  // );
   // $(".category__parent").hover(
   //   function () {
   //     $(".category__child").show(); 
-  //     $(this).append($(".category__child"));
+  //     // $(this).append($(".category__child"));
   //   },
   //   function () {
   //     $(".category__child").hide();
@@ -29,9 +27,9 @@ $(function(){
   $(".category__parent").hover(
     function () {
       var idname = $(this).attr('id');
-      console.log(idname);
-      $("#child--"+idname).show(); 
-      $(this).append($("#child--"+idname));
+      // console.log(idname);
+      
+      $(".child--"+idname).show(); 
     },
     function () {
       $(".category__child").hide(); 
@@ -40,8 +38,11 @@ $(function(){
 
   $(".category__child").hover(
     function () {
-      $(".category__grandchild").show(); 
-      $(this).append($(".category__grandchild"));
+      var parent_id = $(this).parents('.category__parent').attr('id');
+      var child_id = $(this).attr('id');
+      console.log(child_id);
+      
+      $(".grandchild--"+parent_id+"/"+child_id).show(); 
     },
     function () {
       $(".category__grandchild").hide();
