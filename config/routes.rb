fafcf4afcedc0459fc16c6e7b90d72b41bbf2a6a
kao37 <4107937@gmail.com>
2020-06-17 12:09:59 +0900

    add~とりあえず保存(購入機能実装後もう一回見直す)

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,8 @@ Rails.application.routes.draw do
 
   resources :cards, only: [:new, :show, :destroy] do
     collection do
-      post 'create', to: 'cards#create'
+      post 'create'
+      # post 'create', to: 'cards#create'
     end
   end
   
