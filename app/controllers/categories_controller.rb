648accf238c1322af511c5fcaf26b83562491363
fwyk-kyb22 <fwrtkybrng22@gmail.com>
2020-06-19 12:38:08 +0900

    パンくずをgretelからbreadcrumbs_on_railsに変更
    パンくずをgretelからbreadcrumbs_on_railsに変更しました。
    中にインスタンス変数を使いたかったため。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,13 @@
 class CategoriesController < ApplicationController
   def index
+    add_breadcrumb "トップページ",  root_path
+    add_breadcrumb "カテゴリー一覧",  categories_path
   end
   def show
     @category = Category.find(params[:id])
     @items = Item.where(category_id: @category.id).includes(:images).order(updated_at: "DESC")
+    add_breadcrumb "トップページ",  root_path
+    add_breadcrumb "カテゴリー一覧",  categories_path
+    add_breadcrumb "#{@category.name}", "#{@category.id}"
   end
 end
