5f399c1d579cb4dac45157e98d62633a61c23d04
s-ss-sss <64889991+s-ss-sss@users.noreply.github.com>
2020-06-21 21:35:00 +0900

    ブランド入力機能

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+Brand.create(name:  'シャネル')
+Brand.create(name:  'サンローラン')
+Brand.create(name:  'クロムハーツ')
+Brand.create(name:  'ロレックス')
